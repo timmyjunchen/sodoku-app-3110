39468eadb274db8c19763aaa7ec7843756688a23
Alex <ajk338@cornell.edu>
2023-03-19 16:14:33 -0400

    testing gitignore

---
+++ src/state.mli
@@ -0,0 +1,5 @@
+(** Representation of dynamic sudoku state.
+
+    This module represents the state of the sudoku grid as it is being played,
+    including the player's current grid, the boxes that have been filled, and
+    functions that cause the state to change. *)
