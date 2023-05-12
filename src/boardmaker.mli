04eb9d9a3cd1805a8463ddfadccf47f13d9a42a3
Mihir Mishra <mrm367@cornell.edu>
2023-05-12 01:00:12 -0400

    added board_filled

--- src/boardmaker.mli
+++ src/boardmaker.mli
@@ -2,3 +2,4 @@ val shuffle : int list -> int list
 val generate_random_diagonal : unit -> int array array
 val generate_board : unit -> int array array
 val valid_place : int array array -> int -> int -> int -> bool
+val board_filled : int array array -> bool
