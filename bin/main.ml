d0b8678fd1b0444f0ad4e7ae3a131a9d0f0c0de8
Alex <ajk338@cornell.edu>
2023-05-14 20:09:36 -0400

    Updated main to use board generation

--- bin/main.ml
+++ bin/main.ml
@@ -1,20 +1,9 @@
 open Sudoku
 open State
+open Boardmaker
 (* open Command *)
 
-let board1_grid =
-  [|
-    [| 2; 0; 0; 3; 0; 0; 0; 0; 0 |];
-    [| 8; 0; 4; 0; 6; 2; 0; 0; 3 |];
-    [| 0; 1; 3; 8; 0; 0; 2; 0; 0 |];
-    [| 0; 0; 0; 0; 2; 0; 3; 9; 0 |];
-    [| 5; 0; 7; 0; 0; 0; 6; 2; 1 |];
-    [| 0; 3; 2; 0; 0; 6; 0; 0; 0 |];
-    [| 0; 2; 0; 0; 0; 9; 1; 4; 0 |];
-    [| 6; 0; 1; 2; 5; 0; 8; 0; 9 |];
-    [| 0; 0; 0; 0; 0; 1; 0; 0; 2 |];
-  |]
-
+let board1_grid = generate_board 9 2
 let board1 = init_state board1_grid
 
 (** [main ()] prompts for the game to play, then starts it. *)
